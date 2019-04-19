0d136e44f9f512eb2b8576d9a73b8efe3dbd49a2
Eugene Tsakh <onatolich@gmail.com>
2019-04-19 12:54:03 -0300

    Add options fallback

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const GuidedSaga = require('./GuidedSaga');
 
 function guideSaga(saga, options) {
-  return new GuidedSaga(saga, options);
+  return new GuidedSaga(saga, { ...options });
 }
 
 module.exports = guideSaga;
